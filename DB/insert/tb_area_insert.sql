7327d23cd42259ca5bd99b42dca5eaebff071a76
PeanoRen <renqinhu@gmail.com>
2018-08-13 23:37:11 +0800

    数据库插入数据

---
+++ DB/insert/tb_area_insert.sql
@@ -0,0 +1,11 @@
+use o2o;
+
+SELECT * FROM o2o.tb_area;
+
+SELECT area_id, area_name, priority, create_time, last_edit_time
+FROM tb_area
+ORDER BY priority DESC;
+        
+
+INSERT INTO `o2o`.`tb_atb_arearea`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(2, '东苑', 1, null, null);
+INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(3, '西苑', 2, null, null);
