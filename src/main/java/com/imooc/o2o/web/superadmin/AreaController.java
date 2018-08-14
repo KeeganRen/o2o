03be261545a3fe0c49bea3e63a67df6e7104d6d8
PeanoRen <renqinhu@gmail.com>
2018-08-14 22:49:19 +0800

    添加controller层，并使用网页和postMan验证连通性http://localhost:8080/o2o/superadmin/listarea

---
+++ src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
@@ -0,0 +1,69 @@
+/**   
+ * Copyright © 2018 ICBC. All rights reserved.
+ * 
+ * 功能描述：
+ * @Package: com.imooc.o2o.web.superadmin 
+ * @author: KeeganRen   
+ * @date: 2018年8月14日 下午6:58:21 
+ */
+package com.imooc.o2o.web.superadmin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.imooc.o2o.entity.Area;
+import com.imooc.o2o.service.AreaService;
+
+/**   
+* Copyright: Copyright (c) 2018 ICBC。 All rights reserved.
+* 
+* @ClassName: AreaController.java
+* @Description: 区域-Controller
+*
+* @version: v1.0.0
+* @author: KeeganRen
+* @date: 2018年8月14日 下午6:58:21 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2018年8月14日     KeeganRen           v1.0.0               修改原因
+*/
+@Controller
+@RequestMapping("/superadmin")
+public class AreaController {
+
+	@Autowired
+	private AreaService areaService;
+	
+	// 访问主目录的方法
+	@RequestMapping(value = "/listarea", method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> listArea() {
+		
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		List<Area> listArea = new ArrayList<Area>();
+		
+		try {
+			listArea = areaService.getAreaList();
+			modelMap.put("success", true);
+			modelMap.put("errMsg", null);
+			modelMap.put("rows", listArea);
+			modelMap.put("total", listArea.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.toString());
+		}
+		
+		return modelMap;
+	}
+}
