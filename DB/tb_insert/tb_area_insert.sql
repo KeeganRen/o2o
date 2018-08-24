a98dfda25539d341a8dd24e93f2a2c31cb3f95dd
PeanoRen <renqinhu@gmail.com>
2018-08-25 00:19:38 +0800

    修改一些遗留错误，sql和server test位置

--- DB/tb_insert/tb_area_insert.sql
+++ DB/tb_insert/tb_area_insert.sql
@@ -7,5 +7,5 @@ FROM tb_area
 ORDER BY priority DESC;
         
 
-INSERT INTO `o2o`.`tb_atb_arearea`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(2, '东苑', 1, null, null);
+INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(2, '东苑', 1, null, null);
 INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(3, '西苑', 2, null, null);
