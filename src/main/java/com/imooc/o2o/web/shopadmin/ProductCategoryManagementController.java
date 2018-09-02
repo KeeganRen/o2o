29e771a8f9380e02b8e815b955c7a2af3a92a830
PeanoRen <renqinhu@gmail.com>
2018-09-03 00:33:20 +0800

    前端-商品类别批量添加

--- src/main/java/com/imooc/o2o/web/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ProductCategoryManagementController.java
@@ -8,20 +8,25 @@
  */
 package com.imooc.o2o.web.shopadmin;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.imooc.o2o.dto.ProductCategoryExecution;
 import com.imooc.o2o.dto.Result;
 import com.imooc.o2o.entity.ProductCategory;
 import com.imooc.o2o.entity.Shop;
 import com.imooc.o2o.enums.ProductCategoryStateEnum;
+import com.imooc.o2o.exceptions.ProductCategoryOperationException;
 import com.imooc.o2o.service.ProductCategoryService;
 
 /**   
@@ -54,4 +59,35 @@ public class ProductCategoryManagementController {
 		}
 	}
 	
+	@RequestMapping(value = "/addproductcategorys", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> addProductCategorys(@RequestBody List<ProductCategory> productCategories, HttpServletRequest request) {
+		
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+		
+		for (ProductCategory pCategory : productCategories) {
+			pCategory.setShopId(currentShop.getShopId());
+		}
+		if (productCategories != null && productCategories.size() > 0) {
+			try {
+				ProductCategoryExecution pExecution = productCategoryService.batchAddProductCategory(productCategories);
+				if (pExecution.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pExecution.getStateInfo());
+				}
+			} catch (ProductCategoryOperationException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请至少输入一个商品类别");
+		}
+		
+		return modelMap;
+	}
 }
