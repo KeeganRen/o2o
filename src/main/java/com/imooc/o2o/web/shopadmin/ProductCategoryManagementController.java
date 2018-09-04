ea1a3588fe2111e3cf18e3594636b42eb30a3c7c
PeanoRen <renqinhu@gmail.com>
2018-09-04 23:20:12 +0800

    后端-Controller层-删除商品类别

--- src/main/java/com/imooc/o2o/web/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ProductCategoryManagementController.java
@@ -90,4 +90,32 @@ public class ProductCategoryManagementController {
 		
 		return modelMap;
 	}
+	
+	@RequestMapping(value = "/removeproductcategory", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> removeProductCategory(Long productCategoryId, HttpServletRequest request) {
+		
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		if (productCategoryId != null && productCategoryId > 0) {
+			try {
+				Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+				ProductCategoryExecution pExecution = productCategoryService.deleteProductCategory(productCategoryId, currentShop.getShopId());
+				if (pExecution.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pExecution.getStateInfo());
+				}
+			} catch (ProductCategoryOperationException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请至少选择一个商品类别");
+		}
+		
+		return modelMap;
+	}
 }
