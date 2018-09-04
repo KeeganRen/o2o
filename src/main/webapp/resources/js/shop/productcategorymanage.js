53856e0c730c099827103e9f069b370435b12584
PeanoRen <renqinhu@gmail.com>
2018-09-05 00:12:29 +0800

    前端-删除商品类别

--- src/main/webapp/resources/js/shop/productcategorymanage.js
+++ src/main/webapp/resources/js/shop/productcategorymanage.js
@@ -19,7 +19,7 @@ $(function(){
 					var tempHtml = '';
 					dataList.map(function(item, index) {
 						tempHtml += ''
-							+ '<div class="row row-product-category">'
+							+ '<div class="row row-product-category now">'
 							+ '<div class="col-33 product-category-name">'
 							+ item.productCategoryName
 							+ '</div>'
@@ -72,4 +72,30 @@ $(function(){
 		});
 	});
 	
+	$('.category-wrap').on('click','.row-product-category.temp .delete', function(e){
+		console.log($(this).parent().parent());
+		$(this).parent().parent().remove();
+	});
+	
+	$('.category-wrap').on('click','.row-product-category.now .delete', function(e){
+		var target = e.currentTarget;
+		$.confirm('确定么?', function(){
+			$.ajax({
+				url : deleteUrl,
+				type : 'POST',
+				data : {
+					productCategoryId : target.dataset.id
+				},
+				dataType : 'json',
+				success : function(data) {
+					if(data.success) {
+						$.toast('删除成功!');
+						getList();
+					} else {
+						$.toast('删除失败!');
+					}
+				}
+			});
+		});
+	});
 });
