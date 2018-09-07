b31b54e702d8d47fb55be38b0b9b79a4002bd1bb
PeanoRen <renqinhu@gmail.com>
2018-09-07 23:16:43 +0800

    添加建表、添加测试数据SQL

---
+++ DB/o2oDBSQL_1.0.sql
@@ -0,0 +1,265 @@
+-- mysql  Ver 14.14 Distrib 5.7.23, for Linux (x86_64) using  EditLine wrapper
+--
+-- Host: localhost    Database: o2odb
+-- ------------------------------------------------------
+-- Server version	5.7.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_area`
+--
+
+DROP TABLE IF EXISTS `tb_area`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_area`(
+	`area_id` int(2) NOT NULL AUTO_INCREMENT,
+    `area_name` varchar(200) NOT NULL,
+    `priority` int(2) NOT NULL DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+    `last_edit_time` datetime DEFAULT NULL,
+    primary key (`area_id`),
+    unique key `UK_AREA`(`area_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_area`
+--
+
+LOCK TABLES `tb_area` WRITE;
+/*!40000 ALTER TABLE `tb_area` DISABLE KEYS */;
+INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(2, '东苑', 1, null, null);
+INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(3, '西苑', 2, null, null);
+/*!40000 ALTER TABLE `tb_area` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_head_line`
+--
+
+DROP TABLE IF EXISTS `tb_head_line`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_head_line`(
+	`line_id` int(100) NOT NULL AUTO_INCREMENT,
+    `line_name` varchar(1000) DEFAULT NULL,
+    `line_link` varchar(2000) NOT NULL,
+    `line_img` varchar(2000) NOT NULL,
+    `priority` int(2) DEFAULT NULL,
+    `enable_status` int(2) NOT NULL DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+    `last_edit_time` datetime DEFAULT NULL,
+    primary key (`line_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_person_info`
+--
+
+DROP TABLE IF EXISTS `tb_person_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_person_info`(
+	`user_id` int(10) NOT NULL AUTO_INCREMENT,
+    `name` varchar(32) DEFAULT NULL,
+    `profile_img` varchar(1024) DEFAULT NULL,
+    `email` varchar(1024) DEFAULT NULL,
+    `gender` varchar(2) DEFAULT NULL,
+    `enable_status` varchar(2) NOT NULL DEFAULT '0' COMMENT '0:禁止使用本商城, 1:允许使用本商城',
+    `user_type` varchar(2) NOT NULL DEFAULT '1' COMMENT '1:顾客, 2:店家, 3:超级管理员',
+    `create_time` datetime DEFAULT NULL,
+    `last_edit_time` datetime DEFAULT NULL,
+    primary key (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_person_info`
+--
+
+LOCK TABLES `tb_person_info` WRITE;
+/*!40000 ALTER TABLE `tb_person_info` DISABLE KEYS */;
+INSERT INTO `o2o`.`tb_person_info` (`name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('测试', 'test', 'test', '1', '1', '2');
+INSERT INTO `o2o`.`tb_person_info` (`name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('测试2', 'test', 'test', '1', '1', '2');
+INSERT INTO `o2o`.`tb_person_info` (`name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('任亲虎', 'img', 'renqinhu@gmail.com', '1', '1', '2');
+
+/*!40000 ALTER TABLE `tb_person_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_product`
+--
+
+DROP TABLE IF EXISTS `tb_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_product`(
+	`product_id` int(100) NOT NULL AUTO_INCREMENT,
+    `product_name` varchar(100) NOT NULL,
+	`product_desc` varchar(2000) DEFAULT NULL,
+    `img_addr` varchar(2000) DEFAULT '',
+    `normal_price` varchar(100) DEFAULT NULL,
+    `promotion_price` varchar(100) DEFAULT NULL,
+	`priority` int(2) NOT NULL DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+	`last_edit_time` datetime DEFAULT NULL,
+    `enable_status` int(2) NOT NULL DEFAULT '0',
+    `product_category_id` int(11) DEFAULT NULL,
+    `shop_id` int(20) NOT NULL DEFAULT '0',
+    primary key (`product_id`),
+    constraint `fk_product_procate` foreign key(`product_category_id`) references `tb_product_category`(`product_category_id`),
+    constraint `fk_product_product` foreign key(`shop_id`) references `tb_shop`(`shop_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_product_category`
+--
+
+DROP TABLE IF EXISTS `tb_product_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_product_category`(
+	`product_category_id` int(11) NOT NULL AUTO_INCREMENT,
+    `product_category_name` varchar(100) NOT NULL,
+	`priority` int(2) DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+    `shop_id` int(20) NOT NULL DEFAULT '0',
+    primary key (`product_category_id`),
+    constraint `fk_procate_shop` foreign key(`shop_id`) references `tb_shop`(`shop_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_product_category`
+--
+
+LOCK TABLES `tb_product_category` WRITE;
+/*!40000 ALTER TABLE `tb_product_category` DISABLE KEYS */;
+INSERT INTO `o2o`.`tb_product_category` (`product_category_id`, `product_category_name`, `priority`, `shop_id`) VALUES ('1', '面条', '0', '1');
+INSERT INTO `o2o`.`tb_product_category` (`product_category_id`, `product_category_name`, `priority`, `shop_id`) VALUES ('2', '米饭', '20', '1');
+INSERT INTO `o2o`.`tb_product_category` (`product_category_id`, `product_category_name`, `priority`, `shop_id`) VALUES ('3', '粥', '2', '1');
+INSERT INTO `o2o`.`tb_product_category` (`product_category_id`, `product_category_name`, `priority`, `shop_id`) VALUES ('4', '炒饭', '5', '2');
+/*!40000 ALTER TABLE `tb_product_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_product_img`
+--
+
+DROP TABLE IF EXISTS `tb_product_img`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_product_img`(
+	`product_img_id` int(20) NOT NULL AUTO_INCREMENT,
+    `img_addr` varchar(2000) NOT NULL,
+    `img_desc` varchar(2000) NOT NULL,
+	`priority` int(2) DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+    `product_id` int(20) DEFAULT NULL,
+    primary key (`product_img_id`),
+    constraint `fk_proimg_product` foreign key(`product_id`) references `tb_product`(`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_shop`
+--
+
+DROP TABLE IF EXISTS `tb_shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_shop`(
+	`shop_id` int(10) NOT NULL AUTO_INCREMENT,
+    `owner_id`  int(10) NOT NULL COMMENT '店铺创建人',
+    `area_id` int(5) DEFAULT NULL,
+    `shop_category_id` int(11) DEFAULT NULL,
+    `shop_name` varchar(256) NOT NULL,
+    `shop_desc` varchar(1024) DEFAULT NULL,
+    `shop_addr` varchar(200) DEFAULT NULL,
+    `phone` varchar(128) DEFAULT NULL,
+    `shop_img` varchar(1024) DEFAULT NULL,
+    `priority` int(3) DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+    `last_edit_time` datetime DEFAULT NULL,
+    `enable_status` int(2) NOT NULL DEFAULT '0',
+    `advice` varchar(255) DEFAULT NULL,
+    primary key (`shop_id`),
+    constraint `fk_shop_area` foreign key(`area_id`) references `tb_area`(`area_id`),
+    constraint `fk_shop_profile` foreign key(`owner_id`) references `tb_person_info`(`user_id`),
+    constraint `fk_shop_shopcate` foreign key(`shop_category_id`) references `tb_shop_category`(`shop_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tb_shop_category`
+--
+
+DROP TABLE IF EXISTS `tb_shop_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_shop_category`(
+	`shop_category_id` int(11) NOT NULL AUTO_INCREMENT,
+    `shop_category_name` varchar(100) DEFAULT NULL,
+    `shop_category_desc` varchar(1000) NOT NULL,
+    `shop_category_img` varchar(2000) NOT NULL,
+    `priority` int(2) NOT NULL DEFAULT '0',
+    `create_time` datetime DEFAULT NULL,
+    `last_edit_time` datetime DEFAULT NULL,
+    `parent_id` int(11) DEFAULT NULL,
+    primary key (`shop_category_id`),
+    constraint `fk_shop_category_self` foreign key(`parent_id`) references `tb_shop_category`(`shop_category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_shop_category`
+--
+
+LOCK TABLES `tb_shop_category` WRITE;
+/*!40000 ALTER TABLE `tb_shop_category` DISABLE KEYS */;
+INSERT INTO `o2o`.`tb_shop_category` (`shop_category_id`, `shop_category_name`, `shop_category_desc`, `shop_category_img`, `priority`) VALUES ('1', '咖啡奶茶', '咖啡奶茶', 'test', '1');
+INSERT INTO `o2o`.`tb_shop_category` (`shop_category_id`, `shop_category_name`, `shop_category_desc`, `shop_category_img`, `priority`, `parent_id`) VALUES ('2', '咖啡', '测试类别', 'test3', '0', '1');
+/*!40000 ALTER TABLE `tb_shop_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_wechat_auth`
+--
+
+DROP TABLE IF EXISTS `tb_wechat_auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+create table `tb_wechat_auth`(
+	`wechat_auth_id` int(10) NOT NULL AUTO_INCREMENT,
+    `user_id` int(10) NOT NULL,
+    `open_id` varchar(1024) NOT NULL,
+    `create_time` datetime DEFAULT NULL,
+    primary key (`wechat_auth_id`),
+    constraint `fk_wechatauth_profile` foreign key(`user_id`) references `tb_person_info`(`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+alter table tb_wechat_auth add unique index(open_id);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-07 23:06:03
