7b8b79a18d2836e73044f828d54f018fc36833a0
KeeganRen <renqinhu@gmail.com>
2018-10-17 23:19:37 +0800

    [ 数据库]更新数据库插入数据 tb_shop_category

--- DB/o2oDBSQL_1.0.sql
+++ DB/o2oDBSQL_1.0.sql
@@ -251,9 +251,29 @@ create table `tb_shop_category`(
 
 LOCK TABLES `tb_shop_category` WRITE;
 /*!40000 ALTER TABLE `tb_shop_category` DISABLE KEYS */;
-INSERT INTO `o2o`.`tb_shop_category` (`shop_category_id`, `shop_category_name`, `shop_category_desc`, `shop_category_img`, `priority`) VALUES ('1', '咖啡奶茶', '咖啡奶茶', 'test', '1');
-INSERT INTO `o2o`.`tb_shop_category` (`shop_category_id`, `shop_category_name`, `shop_category_desc`, `shop_category_img`, `priority`, `parent_id`) VALUES ('2', '咖啡', '测试类别', 'test3', '0', '1');
+LOCK TABLES `tb_shop_category` WRITE;
+/*!40000 ALTER TABLE `tb_shop_category` DISABLE KEYS */;
+INSERT INTO `tb_shop_category` VALUES 
+(10,'二手市场','二手商品交易','/upload/item/shopcategory/2017061223272255687.png',100,'2017-06-04 20:10:58','2017-06-12 23:27:22',NULL),
+(11,'美容美发','美容美发','/upload/item/shopcategory/2017061223273314635.png',99,'2017-06-04 20:12:57','2017-06-12 23:27:33',NULL),
+(12,'美食饮品','美食饮品','/upload/item/shopcategory/2017061223274213433.png',98,'2017-06-04 20:15:21','2017-06-12 23:27:42',NULL),
+(13,'休闲娱乐','休闲娱乐','/upload/item/shopcategory/2017061223275121460.png',97,'2017-06-04 20:19:29','2017-06-12 23:27:51',NULL),
+(14,'旧车','旧车','/upload/item/shopcategory/2017060420315183203.png',80,'2017-06-04 20:31:51','2017-06-04 20:31:51',10),
+(15,'二手书籍','二手书籍','/upload/item/shopcategory/2017060420322333745.png',79,'2017-06-04 20:32:23','2017-06-04 20:32:23',10),
+(17,'护理','护理','/upload/item/shopcategory/2017060420372391702.png',76,'2017-06-04 20:37:23','2017-06-04 20:37:23',11),
+(18,'理发','理发','/upload/item/shopcategory/2017060420374775350.png',74,'2017-06-04 20:37:47','2017-06-04 20:37:47',11),
+(20,'大排档','大排档','/upload/item/shopcategory/2017060420460491494.png',59,'2017-06-04 20:46:04','2017-06-04 20:46:04',12),
+(22,'奶茶店','奶茶店','/upload/item/shopcategory/2017060420464594520.png',58,'2017-06-04 20:46:45','2017-06-04 20:46:45',12),
+(24,'密室逃生','密室逃生','/upload/item/shopcategory/2017060420500783376.png',56,'2017-06-04 20:50:07','2017-06-04 21:45:53',13),
+(25,'KTV','KTV','/upload/item/shopcategory/2017060420505834244.png',57,'2017-06-04 20:50:58','2017-06-04 20:51:14',13),
+(27,'培训教育','培训教育','/upload/item/shopcategory/2017061223280082147.png',96,'2017-06-04 21:51:36','2017-06-12 23:28:00',NULL),
+(28,'租赁市场','租赁市场','/upload/item/shopcategory/2017061223281361578.png',95,'2017-06-04 21:53:52','2017-06-12 23:28:13',NULL),
+(29,'程序设计','程序设计','/upload/item/shopcategory/2017060421593496807.png',50,'2017-06-04 21:59:34','2017-06-04 21:59:34',27),
+(30,'声乐舞蹈','声乐舞蹈','/upload/item/shopcategory/2017060421595843693.png',49,'2017-06-04 21:59:58','2017-06-04 21:59:58',27),
+(31,'演出道具','演出道具','/upload/item/shopcategory/2017060422114076152.png',45,'2017-06-04 22:11:40','2017-06-04 22:11:40',28),
+(32,'交通工具','交通工具','/upload/item/shopcategory/2017060422121144586.png',44,'2017-06-04 22:12:11','2017-06-04 22:12:11',28);
 /*!40000 ALTER TABLE `tb_shop_category` ENABLE KEYS */;
+UNLOCK TABLES;/*!40000 ALTER TABLE `tb_shop_category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
