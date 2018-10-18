d925e1ba40dcf02dffa70ddcd503bdef1d55b904
KeeganRen <renqinhu@gmail.com>
2018-10-19 01:09:20 +0800

    [数据库]更新数据库表的测试数据tb_area\ tb_person_info \ tb_shop \

--- DB/o2oDBSQL_1.0.sql
+++ DB/o2oDBSQL_1.0.sql
@@ -39,8 +39,18 @@ create table `tb_area`(
 
 LOCK TABLES `tb_area` WRITE;
 /*!40000 ALTER TABLE `tb_area` DISABLE KEYS */;
-INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(2, '东苑', 1, null, null);
-INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(3, '西苑', 2, null, null);
+INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(1, '小和山', 1, null, null);
+INSERT INTO `o2o`.`tb_area`(`area_id`, `area_name`, `priority`, `create_time`, `last_edit_time`) VALUES(2, '中', 2, null, null);
+/*!40000 ALTER TABLE `tb_area` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `tb_area` WRITE;
+/*!40000 ALTER TABLE `tb_area` DISABLE KEYS */;
+INSERT INTO `tb_area` VALUES 
+(3,'东苑',12,'2017-06-04 19:12:58','2017-06-04 19:12:58'),
+(4,'南苑',10,'2017-06-04 19:13:09','2017-06-04 19:13:09'),
+(5,'西苑',9,'2017-06-04 19:13:18','2017-06-04 19:13:18'),
+(6,'北苑',7,'2017-06-04 19:13:29','2017-06-04 19:13:29');
 /*!40000 ALTER TABLE `tb_area` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,6 +135,10 @@ LOCK TABLES `tb_person_info` WRITE;
 INSERT INTO `o2o`.`tb_person_info` (`name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('测试', 'test', 'test', '1', '1', '2');
 INSERT INTO `o2o`.`tb_person_info` (`name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('测试2', 'test', 'test', '1', '1', '2');
 INSERT INTO `o2o`.`tb_person_info` (`name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('任亲虎', 'img', 'renqinhu@gmail.com', '1', '1', '2');
+INSERT INTO `o2o`.`tb_person_info` (`user_id`, `name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('8', '李翔', 'http://wx.qlogo.cn/mmopen/XZumId0qMA815ApfWI2zibDnRMahic6SU0wHib2HgGJj5narL2ymRaI4Kn2Tx2Q8UfkicibvjVicu3De6fDYRMfo0uGW0SGicibxVnJ9/0', 'renqinhu@gmail.com', '1', '1', '2');
+INSERT INTO `o2o`.`tb_person_info` (`user_id`, `name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('9', '龙州一条街客服', 'http://wx.qlogo.cn/mmopen/icF4iau8Sj7b0FiakC6ibBoTPmkvLpIX9YhWkNyEIGYfzYyqBiag2M3q2rnxSlXAh95UDHdWgywvEW5bN5FBzFPFazxBzqHTRqNwn/0', 'renqinhu@gmail.com', '1', '1', '2');
+INSERT INTO `o2o`.`tb_person_info` (`user_id`, `name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('10', 'king', 'http://wx.qlogo.cn/mmopen/XZumId0qMA815ApfWI2zibDLckaAaV6QgcBJP0saJSDTuicZBd35HzPXUebLPSlexCIPJsLs3w6lG0xmwn3EZNicj04dJh4We7C/0', 'renqinhu@gmail.com', '0', '1', '2');
+INSERT INTO `o2o`.`tb_person_info` (`user_id`, `name`, `profile_img`, `email`, `gender`, `enable_status`, `user_type`) VALUES ('11', '音策', 'http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKCWfIBicEwS3U0legxxQd5XFpZibBXVPyz0wphvvtaXqiblzQF2GqE28c7j8FGpuYqBCg1QRJThEzuw/0', 'renqinhu@gmail.com', '1', '1', '2');
 
 /*!40000 ALTER TABLE `tb_person_info` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -233,6 +247,29 @@ create table `tb_shop`(
     constraint `fk_shop_shopcate` foreign key(`shop_category_id`) references `tb_shop_category`(`shop_category_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+LOCK TABLES `tb_shop` WRITE;
+/*!40000 ALTER TABLE `tb_shop` DISABLE KEYS */;
+INSERT INTO `tb_shop` VALUES 
+(15,8,3,14,'二手车辆','二手汽车、摩托车、电车等交通工具交易信息。','面向全市','0000000','/upload/item/shop/15/2017060522042982266.png',100,'2017-06-05 22:04:29','2017-08-25 10:50:16',1,NULL),
+(16,8,3,15,'旧书籍交易','旧书籍交易信息','旧书籍交易板块','0000000','/upload/item/shop/16/2017060608534289617.png',99,'2017-06-06 08:53:42','2017-06-06 08:54:40',1,NULL),
+(17,8,3,17,'靓仔靓妹美容护理中心','二十年手艺，专业护理秀发受损头发。美容美发首选。','东苑北面二号门','4445556','/upload/item/shop/17/2017060609084595067.jpg',0,'2017-06-06 09:08:45','2017-06-06 09:45:32',1,NULL),
+(18,8,3,18,'一剪没理发中心','专业洗剪吹，又好又便宜。','东苑北面3号门面','9998887','/upload/item/shop/18/2017060609110899956.jpg',0,'2017-06-06 09:11:08','2017-06-06 09:45:38',1,NULL),
+(19,8,4,20,'吃得饱大排档','吃得好又吃得饱，朋友聚会好地方。可预约。','南苑东面10号门面','1234567','/upload/item/shop/19/2017060609140699548.jpg',0,'2017-06-06 09:14:06','2017-06-06 09:45:43',1,NULL),
+(20,8,4,22,'香喷喷奶茶店','鲜榨果汁、奶茶等饮品。','南苑东面5号门面','77788444','/upload/item/shop/20/2017060609163395401.jpg',30,'2017-06-06 09:16:33','2017-06-07 16:24:07',1,'\"\"'),
+(21,8,5,25,'海陆空量贩KTV','订包厢电话：8889997。节假日请预约。','西苑1号门面','8889997','/upload/item/shop/21/2017060609194286080.jpg',0,'2017-06-06 09:19:42','2017-06-06 09:45:59',1,NULL),
+(22,8,5,24,'幽城室逃生娱乐城','考验你的智商，和小伙伴们一起来挑战吧。','西苑3号楼第二层','6666333','/upload/item/shop/22/2017060609223853062.jpg',0,'2017-06-06 09:22:38','2017-06-06 09:46:04',1,NULL),
+(23,8,6,29,'威水程序设计培训教育','保教抱会，前途无量。','北苑2栋5楼','66633111','/upload/item/shop/23/2017060609275777519.png',0,'2017-06-06 09:27:57','2017-06-06 09:46:09',1,NULL),
+(24,8,6,30,'武林风舞蹈培训','专业培训舞蹈，声乐。','北苑9懂10楼','5555555','/upload/item/shop/24/2017060609354459045.png',0,'2017-06-06 09:35:44','2017-06-06 09:46:13',1,NULL),
+(25,8,6,14,'易行交通工具租赁服务中心','本店租赁各种汽车，摩托车等。详情请拨打电话咨询。电话：2222222','1栋3号4号门面','2222222','/upload/item/shop/25/2017060609381150709.png',40,'2017-06-06 09:38:11','2017-06-06 19:58:32',1,NULL),
+(26,8,6,31,'有声有色','出租各种演出道具，乐器，服装等。','北苑15号门面','7777777','/upload/item/shop/26/2017060609431259039.png',41,'2017-06-06 09:43:12','2017-06-06 19:58:45',1,NULL),
+(27,8,3,22,'冰冻夏天奶茶店','本店出售各种冷饮，奶茶，冰花，鲜榨果汁。','东苑7懂2号门面','8889999','/upload/item/shop/27/2017060715512185473.jpg',10,'2017-06-07 15:51:21','2017-06-07 16:22:28',1,'\"\"'),
+(28,9,3,14,'test','dfafaf','sdafafafa','3424242','/upload/item/shop/28/2017082500103690946.png',0,'2017-08-25 00:10:36','2017-08-25 00:10:36',0,NULL);
+/*!40000 ALTER TABLE `tb_shop` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 --
 -- Table structure for table `tb_shop_category`
--- Server/README.md
+++ Server/README.md
@@ -3,7 +3,7 @@
 ## Tomcat配置
 server.xml配置
 
-		1.
+		1.o2o链路
       	<Context docBase="o2o" path="/o2o" reloadable="true" source="org.eclipse.jst.jee.server:o2o"/>
       	
       	2.前端首页加载图片链路
